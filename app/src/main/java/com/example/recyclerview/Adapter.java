a0918f255862fe5ed75d2694c568adfd29811559
Khazaina <shkhazainahayee@gmail.com>
2021-12-24 19:49:35 +0500

    adapter

--- app/src/main/java/com/example/recyclerview/Adapter.java
+++ app/src/main/java/com/example/recyclerview/Adapter.java
@@ -23,7 +23,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_design,parent,false);
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main,parent,false);
         return new ViewHolder(view);
 
 
@@ -48,6 +48,43 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public int getItemCount() {
         return userList.size();
     }
+
+    //view holder class
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+        private TextView textView;
+        private TextView textView2;
+        private TextView textview3;
+        private TextView divider;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //here use xml ids
+            //give different name not like constructor
+            imageView=itemView.findViewById(R.id.imageview);
+            textView=itemView.findViewById(R.id.textview);
+            textView2=itemView.findViewById(R.id.textview2);
+            textview3=itemView.findViewById(R.id.textview3);
+            divider=itemView.findViewById(R.id.Divider);
+
+
+        }
+
+        public void setData(int resource, String name, String msg, String time,String line) {
+
+            imageView.setImageResource(resource);
+            textView.setText(name);
+            textView2.setText(msg);
+            textview3.setText(time);
+            divider.setText(line);
+
+
+
+        }
 
 
     }
